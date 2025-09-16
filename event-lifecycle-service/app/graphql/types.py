002cd88f123eb1fbe7a1975ad0c727562c649795
Saheed Alpha Mansaray <mansaraysaheedalpha@gmail.com>
2025-09-16 14:50:08 +0000

    still progressing on the event-lifecycle-service

--- event-lifecycle-service/app/graphql/types.py
+++ event-lifecycle-service/app/graphql/types.py
@@ -3,8 +3,22 @@
 import strawberry
 import typing
 from datetime import datetime
+from strawberry.types import Info
 from ..models.event import Event as EventModel
 from ..models.registration import Registration as RegistrationModel
+
+
+@strawberry.type
+class EventsPayload:
+    events: typing.List["EventType"]
+    totalCount: int
+
+
+@strawberry.type
+class EventStatsType:
+    totalEvents: int
+    upcomingEvents: int
+    totalRegistrations: int
 
 
 # This federated User is correct.
@@ -36,31 +50,40 @@ class EventType:
     status: str
     is_archived: bool
 
+    @strawberry.field
+    def imageUrl(self, root: dict) -> typing.Optional[str]:
+        return root["imageUrl"]
+
+    @strawberry.field
+    def registrationsCount(self, root: dict) -> int:
+        # This value is pre-calculated and attached in the CRUD layer
+        return root["registrationsCount"]
+
     # These fields have a camelCase name in the schema but snake_case in the model.
     # We must provide custom resolvers for each of them.
     @strawberry.field
-    def startDate(self, root: EventModel) -> datetime:
-        return root.start_date
+    def startDate(self, root: dict) -> datetime:
+        return root["start_date"]
 
     @strawberry.field
-    def endDate(self, root: EventModel) -> datetime:
-        return root.end_date
+    def endDate(self, root: dict) -> datetime:
+        return root["end_date"]
 
     @strawberry.field
-    def venueId(self, root: EventModel) -> typing.Optional[str]:
-        return root.venue_id
+    def venueId(self, root: dict) -> typing.Optional[str]:
+        return root["venue_id"]
 
     @strawberry.field
-    def isPublic(self, root: EventModel) -> bool:
-        return root.is_public
+    def isPublic(self, root: dict) -> bool:
+        return root["is_public"]
 
     @strawberry.field
-    def createdAt(self, root: EventModel) -> datetime:
-        return root.createdAt  # This one was already camelCase in the model
+    def createdAt(self, root: dict) -> datetime:
+        return root["createdAt"]  # This one was already camelCase in the model
 
     @strawberry.field
-    def updatedAt(self, root: EventModel) -> datetime:
-        return root.updatedAt  # This one was also camelCase
+    def updatedAt(self, root: dict) -> datetime:
+        return root["updatedAt"]  # This one was also camelCase
 
 
 # The rest of the types are correct.
